660aa685923f962a4556bbc81ff51c4cb9aee649
oandreazini <oriol_as_92@hotmail.com>
2022-08-09 12:50:54 +0200

    TA23 Ex1

---
+++ TA23_1/src/main/java/MainApp/Ta231Application.java
@@ -0,0 +1,13 @@
+package MainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta231Application.class, args);
+	}
+
+}
